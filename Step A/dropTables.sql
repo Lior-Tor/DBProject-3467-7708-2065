65ddad6cacf2e1eb609f699c85177f9c4ab589db
Lior <tordjmanlior1000@hotmail.com>
2024-06-03 21:33:55 +0300

    Delete Step A directory

--- Step A/dropTables.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE InventoryUsage;
-DROP TABLE Inventory;
-DROP TABLE Sales;
-DROP TABLE Products;
-DROP TABLE Customers;
-DROP TABLE Staff;
-DROP TABLE Suppliers;
--- Step A/selectAll.sql
+++
@@ -1,7 +0,0 @@
-SELECT * FROM Customers;
-SELECT * FROM Sales;
-SELECT * FROM Products;
-SELECT * FROM Inventory;
-SELECT * FROM Staff;
-SELECT * FROM Suppliers;
-SELECT * FROM InventoryUsage;
