cef677d69d2c849f02fcad0ea505d10c0ccf5d7c
Lior <tordjmanlior1000@hotmail.com>
2024-06-18 17:54:56 +0300

    Add files via upload

---
+++ Step A/DGFiles/PRODUCTS.gd
@@ -0,0 +1,46 @@
+
+[General]
+Version=1
+
+[Preferences]
+Username=
+Password=2949
+Database=
+DateFormat=
+CommitCount=0
+CommitDelay=0
+InitScript=
+
+[Table]
+Owner=SYS
+Name=PRODUCTS
+Count=400
+
+[Record]
+Name=PRODUCTID
+Type=NUMBER
+Size=
+Data=Sequence(10000)
+Master=
+
+[Record]
+Name=PRODUCTNAME
+Type=VARCHAR2
+Size=100
+Data=List('Laptop', 'Dog Food', 'Skin Care', 'Phone', 'Sports Shoes')
+Master=
+
+[Record]
+Name=TYPEOFPRODUCT
+Type=VARCHAR2
+Size=100
+Data=List('Electronics', 'Pet Supplies', 'Beauty', 'Sports Equipment')
+Master=
+
+[Record]
+Name=PRICE
+Type=NUMBER
+Size=
+Data=Random(10, 500)
+Master=
+
