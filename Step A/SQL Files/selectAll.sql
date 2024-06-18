69550e4b29f025164d94329d38f0ad4d4ae96b36
Lior <tordjmanlior1000@hotmail.com>
2024-06-18 17:56:48 +0300

    Add files via upload

---
+++ Step A/SQL Files/selectAll.sql
@@ -0,0 +1,20 @@
+-- Select data from Customers
+SELECT * FROM Customers;
+
+-- Select data from Employees
+SELECT * FROM Employees;
+
+-- Select data from Products
+SELECT * FROM Products;
+
+-- Select data from Suppliers
+SELECT * FROM Suppliers;
+
+-- Select data from Inventory
+SELECT * FROM Inventory;
+
+-- Select data from Sales
+SELECT * FROM Sales;
+
+-- Select data from SalesProducts
+SELECT * FROM SalesProducts;
